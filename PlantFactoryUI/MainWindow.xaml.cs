9033f93a9266af9efc7784d4ea7593deb029540d
Kevin Wang <20143894@stu.neu.edu.cn>
2018-12-17 14:45:20 +1100

    Nothing Tested Start this work today

--- PlantFactoryUI/MainWindow.xaml.cs
+++ PlantFactoryUI/MainWindow.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,6 +24,66 @@ namespace PlantFactoryUI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private SerialPort ComDevice = new SerialPort();
+        public delegate void HandleInterfaceUpdate(string text);
+        private HandleInterfaceUpdate HIU;
+
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ComDevice.PortName = "COM3";
+            ComDevice.BaudRate = 115200;
+            ComDevice.Parity = Parity.None;
+            ComDevice.StopBits = StopBits.One;
+            PortOpen();
+        }
+
+        public bool PortOpen()
+        {
+            ComDevice.DataReceived += new SerialDataReceivedEventHandler(DataReceive);
+            ComDevice.ReceivedBytesThreshold = 1;
+            ComDevice.RtsEnable = true;
+            try
+            {
+                if (!ComDevice.IsOpen)
+                {
+                    ComDevice.Open();
+                }
+            }
+            catch
+            {
+            }
+            if (ComDevice.IsOpen)
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("PortOpen Unsuccessful!!", "ShimaRin Warning", MessageBoxButton.OK);
+                return false;
+            }
+        }
+
+        private void DataReceive(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort SP = (SerialPort)(sender);
+            Thread.Sleep(100);
+            int num = SP.BytesToRead;
+            byte[] readBuffer = new byte[num];
+            SP.Read(readBuffer, 0, num);
+            string str = Encoding.UTF8.GetString(readBuffer);
+            MessageBox.Show(str, "ShimaRin Warning", MessageBoxButton.OK);
+
+            //HIU = new HandleInterfaceUpdate(UpdateTextBox);
+            Dispatcher.Invoke(HIU, new string[] { Encoding.ASCII.GetString(readBuffer) });
+        }
+
+
+        public void PortSend(string Command)
+        {
+            byte[] writeBuffer = Encoding.ASCII.GetBytes(Command);
+            ComDevice.Write(writeBuffer, 0, writeBuffer.Length);
         }
     }
 }
